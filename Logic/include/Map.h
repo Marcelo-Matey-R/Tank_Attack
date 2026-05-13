3fce214ef783cc9828e06689d63603280cc70ff8
Marcelo-Matey-R <queridozapato@gmail.com>
2026-05-12 21:45:59 -0600

    Se cambio el mapa para que utilice una Matriz de la clase Element, donde los elementos pueden ser tanques, obstaculos o pisos, ademas, mapa es quien guarda a los tanques de cada jugador, esto provoco que se refactorizara parte de la clase para eliminar y crear metodos

--- Logic/include/Map.h
+++ Logic/include/Map.h
@@ -1,43 +1,56 @@
 #include "include/Constants.h"
+#include "Element.h"
+#include "StructPosition.h"
+#include "EnumColorTank.h"
+#include "Tank.h"
 #include <map>
 #include <vector>
 #ifndef MAP_H
 #define MAP_H
-class Tank;
+
 class Map{
 	private:
-	int map[MAP_HEIGHT][MAP_WIDTH];
-	std::map<std::pair<int, int>, Tank*> posToTank;
-	std::map<int, std::pair<int, int>> idTankToPos;
+	Element* map[GRID_ROWS][GRID_COLS] = {nullptr};
+	Tank** tanksPlayer1 = new Tank*[2];
+	Tank** tanksPLayer2 = new Tank*[2];
 	Map();
 	static Map* instance;
 
 	void  FillMap(); //Rellena el mapa
 
-	std::vector<int> CreateRows(); //Crea las filas con numeros que van del 0-5, los 4 tipos de Tank, piso y obstaculos
+	void SetTankOfPlayer1(); //Se crean los tanques del jugador 1 y se ponen en una posicion aleatoria del mapa no ocupada
+	void SetTankOfPlayer2(); //Se crean los tanques del jugador 2 y se ponen en una posicion aleatoria del mapa no ocupada
 
 	bool VerifyConectivity(); //Recorre el mapa para verificar que cada Tank pueda llegar a los demas
 
 	void CreateConectivity(); //Si no hay conectividad entonces forzamos que haya
 
-	bool IsFLoor(int row, int col); //Verifica que el valor en la posicion (row, col) sea igual a la del piso
+	bool IsFLoor(const Position &pos); //Verifica que el valor en la posicion (row, col) sea un suelo
+	bool IsObstacle(const Position &pos); //Verifica que el valor en la posicion (row, col) sea un obstaculo
+	bool IsTank(const Position &pos); //Verifica que el valor en la posicion (row, col) sea un tanque
 
 	public:
+	/*
+		Asegurar existencia de un unico mapa en todo el juego
+	*/
 	static Map *GetInstance();
 	Map(const Map&) = delete;
 	void operator=(const Map&) = delete;
 
 	~Map();
-	void EliminateObstacle(int row, int col); //Cambia el valor en la posicion (row, col) al vlaor de piso
 
-	void EliminateTank(int row, int col); //Cambia el valor de la posicion al del piso y elimna el valor de los unordered_map
+	void EliminateObstacle(const Position &pos); // Elimina el obstaculo por un suelo sin tumba
 
-	void MoveTank(Tank* tank, int row, int col); //Intenta mover el tanque a la posicion (row, col) si no se puede manda un error
+	void EliminateTank(const Position &pos); // Elimina el tanque y lo cambia por un floor con tumba
 
-	int GetValue(int row, int col); //Devuelve el valor en la posicion (row, col)
+	void MoveTank(Tank* tank, const Position &destination); // Intenta mover el tanque a la posicion (row, col) si no se puede manda un error
 
-	Tank* GetTankIn(int row, int col); //Devuelve el tanque en (row, col)
+	int GetElementAt(const Position &pos); // Devuelve el valor en la posicion (row, col)
 
-	void SetTank(Tank* tank); //Busca la posicion en base al id del tanque e introduce las cosas al posToTank
+	Tank* GetTankIn(const Position &pos); // Devuelve el tanque en (row, col)
+
+	Tank* GetTankOfPlayer(int idPlayer, ColorTank color); // Busca al tanque de ese color
+	
+	Tank** GetTanksOfPlayer(int idPlayer); // Devuelve la lista con los tanques pertenecientes al jugador
 };
 #endif
