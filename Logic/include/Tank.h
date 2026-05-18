f2989b851b0fd91087edcb7a3c821e5bbca8e7cd
Marcelo-Matey-R <queridozapato@gmail.com>
2026-05-17 18:47:18 -0600

    Se cambio de lugar el SelectPathFinding para que sea un metodo privado

--- Logic/include/Tank.h
+++ Logic/include/Tank.h
@@ -15,49 +15,46 @@ class Tank : public Element{
     int life;
     ColorTank color; // Valor que se utiliza el mapa
     Position position;
-    bool isMovePrecise;
-
+    virtual TypePath SelectPathfinding(PowerUp movePrecise = PowerUp::NULL_POWERUP) = 0; // Da un numero para saber cual pathfinding se eligio (depende del tipo de tanque)
     public:
     Tank(ColorTank color) : Element(TypeElement::Tank){
         this->color = color;
         this->life = 100;
-        this->isMovePrecise = false;
         Position pos = {0, 0};
         SetPosition(pos);
     }
     virtual ~Tank() = default;
 
-    bool SetPosition(const Position &n){
-        if(n.x < 0 || n.y < 0 || n.x >= GRID_COLS || n.y >= GRID_ROWS) return false;
-        position.x = n.x; position.y = n.y;
-        return true;
+    void SetPosition(const Position &n){
+        if(n.i < 0 || n.j < 0 || n.i >= GRID_COLS || n.j >= GRID_ROWS) throw std::invalid_argument("La posicion no puede ser negativa o sobrepasar los limites del mapa");
+        position.i = n.i; position.j = n.j;
     }
-    void SetMovePrecise(bool isPrecise){
-        this->isMovePrecise = isPrecise;
-    }
-    virtual TypePath SelectPathfinding(PowerUp movePrecise = PowerUp::NULL_POWERUP) = 0; // Da un numero para saber cual pathfinding se eligio (depende del tipo de tanque)
+
     virtual void DecreaseHealth(PowerUp attackPlus = PowerUp::NULL_POWERUP) = 0; // Cada tipo de tanque tiene en recibir danio
     virtual Bullet* Attack(PowerUp attackPlus = PowerUp::NULL_POWERUP, PowerUp attackAccuracy = PowerUp::NULL_POWERUP, const Position &destiny) = 0; // Cada tanque ataca a su manera    
-    bool IsAlive(){return life <= 0;} // Dice si el tanque esta vivo o no
+    bool IsAlive(){return life > 0;} // Dice si el tanque esta vivo o no
+    int GetLife(){return life;}
     ColorTank GetColor(){return color;} // Devuelve el color del tanque
     Position GetPosition(){return position;} // Devuelve la posicion
-    std::vector<Position> GetPath(Position destination); // Devuelve el Path para ir al destino
+    std::vector<Position> GetPath(PowerUp movePrecise = PowerUp::NULL_POWERUP, Position destination); // Devuelve el Path para ir al destino
 
 };
 
 // Subclases de tanque
 class TankLightBlueAndBlue : public Tank{
+    private:
+    TypePath SelectPathfinding(PowerUp movePrecise = PowerUp::NULL_POWERUP) override;
     public:
     TankLightBlueAndBlue(ColorTank color) : Tank(color){}
-    TypePath SelectPathfinding(PowerUp movePrecise = PowerUp::NULL_POWERUP) override;
     void DecreaseHealth(PowerUp attackPlus = PowerUp::NULL_POWERUP) override;
     Bullet* Attack(PowerUp attackPlus = PowerUp::NULL_POWERUP, PowerUp attackAccuracy = PowerUp::NULL_POWERUP, const Position &destiny) override;
 };
 
 class TankYellowAndRed : public Tank{
+    private:
+    TypePath SelectPathfinding(PowerUp movePrecise = PowerUp::NULL_POWERUP) override;
     public:
     TankYellowAndRed(ColorTank color) : Tank(color){}
-    TypePath SelectPathfinding(PowerUp movePrecise = PowerUp::NULL_POWERUP) override;
     void DecreaseHealth(PowerUp attackPlus = PowerUp::NULL_POWERUP) override;
     Bullet* Attack(PowerUp attackPlus = PowerUp::NULL_POWERUP, PowerUp attackAccuracy = PowerUp::NULL_POWERUP, const Position &destiny) override;
 };
