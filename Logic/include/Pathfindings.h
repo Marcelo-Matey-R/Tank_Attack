1c274adb0763d3147690bd336ab7390288ce5899
Marcelo-Matey-R <queridozapato@gmail.com>
2026-05-17 18:42:26 -0600

    Se cambiaron los argumento donde el map ahora hace refencia a una matriz y el select algorithm se le puso el mapa, la posicion inicial y la posicion final, Se creo el metodo IsConnected que busca saber si los nodos de la lista de destino estan conectados entre si, se puso uno para saber si una posicion es invalida, una para movimientos aleatorios, la heuristica y el metodo para reconstruir los path que dan los metodos de busqueda y un Flood Fill para ver los elementos conectados

--- Logic/include/Pathfindings.h
+++ Logic/include/Pathfindings.h
@@ -1,20 +1,28 @@
 #include <vector>
+#include <map>
 #include "StructPosition.h"
 #include "Element.h"
 #include "EnumTypePath.h"
+#include "include/Constants.h"
 
 #ifndef PATHFINDING_H
 #define PATHFINDING_H
 
 class Pathfinding{
     private:
-    static std::vector<Position> Bfs(Element* map, const Position &start, const Position &destiny);
-    static std::vector<Position> Dijkstra(Element* map, const Position &start, const Position &destiny);
-    static std::vector<Position> A_Star_Game(Element* map, const Position &start, const Position &destiny);
-    static std::vector<Position> A_Star_Map(Element* map, const Position &start, const Position &destiny);
-    static std::vector<Position> LOS(Element* map, const Position &start, const Position &destiny);
+    static std::vector<Position> ReconstructorPath(Position (*predecessors)[GRID_COLS], const Position &start, const Position &destiny);
+    static int Heuristic(const Position &start, const Position &destiny);
+    static std::vector<Position> Bfs(Element* (&map)[GRID_ROWS][GRID_COLS], const Position &start, const Position &destiny);
+    static std::vector<Position> Dijkstra(Element* (&map)[GRID_ROWS][GRID_COLS], const Position &start, const Position &destiny);
+    static bool IsInvalidPosition(const Position &neighborPos);
+    static void FloodFill(Element* (&map)[GRID_ROWS][GRID_COLS], int (&zones)[GRID_ROWS][GRID_COLS], const Position &start, int actualColor);
+    static std::vector<Position> A_Star_Game(Element* (&map)[GRID_ROWS][GRID_COLS], const Position &start, const Position &destiny);
+    static void RandomMoves(Element* (&map)[GRID_ROWS][GRID_COLS], std::vector<Position> &pos, Position &start);
+    static std::vector<Position> A_Star_Map(Element* (&map)[GRID_ROWS][GRID_COLS], const Position &start, const Position &destiny);
+    static std::vector<Position> LOS(Element* (&map)[GRID_ROWS][GRID_COLS], const Position &start, const Position &destiny);
     public:
-    static std::vector<Position> SelectAlgorithm(TypePath path);
+    static bool IsConected(Element* (&map)[GRID_ROWS][GRID_COLS], const Position destinies[4]);
+    static std::vector<Position> SelectAlgorithm(TypePath path, Element* (&map)[GRID_ROWS][GRID_COLS], const Position &start, const Position &destiny);
 };
 
 #endif
