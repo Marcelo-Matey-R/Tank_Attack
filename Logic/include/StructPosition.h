162343cb1dce3897f98f4296329b0b07eca0dadd
Marcelo-Matey-R <queridozapato@gmail.com>
2026-05-17 18:45:43 -0600

    Se sobrecargaron mas operadores para facilitar su uso en la implementacion de Pathfindings

--- Logic/include/StructPosition.h
+++ Logic/include/StructPosition.h
@@ -2,11 +2,28 @@
 #define STRUCTPOSITION_H
 
 struct Position{
-    int x;
-    int y;
+    int i;
+    int j;
 
     bool operator==(const Position& p) const{
-        return (x == p.x && y == p.y);
+        return (i == p.i && j == p.j);
+    }
+    bool operator!=(const Position& p) const{
+        return (i != p.i && j != p.j);
+    }
+    bool operator<(const Position& p) const{
+        return (i == p.i) ? j < p.j : p.i < i;
+    }
+    Position operator+(const Position& p) const{
+        return Position{i + p.i, j + p.j};
+    }
+    Position operator+=(const Position &p){
+        i += p.i;
+        j += p.j;
+        return *this;
+    }
+    Position operator-(const Position& p) const{
+        return Position{abs(i - p.i), abs(j - p.j)};
     }
 };
 
