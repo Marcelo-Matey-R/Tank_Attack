98a792b1c02705ea1363e6eda7a49756ab3f57e7
Marcelo-Matey-R <queridozapato@gmail.com>
2026-05-19 20:34:45 -0600

    Arreglo en las direcciones

--- Logic/include/Directions.h
+++ Logic/include/Directions.h
@@ -1,20 +1,14 @@
 #include "StructPosition.h"
 
-const Position up = {0, -1};
+const Position up      = {-1,  0};  // decrementa fila
+const Position down    = { 1,  0};  // incrementa fila
+const Position right   = { 0,  1};  // incrementa columna
+const Position left    = { 0, -1};  // decrementa columna
 
-const Position down = {0, 1};
-
-const Position right = {1, 0};
-
-const Position left = {-1, 0};
-
-const Position diagonal1 = up + Position{1, 0};
-
-const Position diagonal2 = up + Position{-1, 0};
-
-const Position diagonal3 = down + Position{1, 0};
-
-const Position diagonal4 = down + Position{-1, 0};
+const Position diagonal1 = up   + right;  // {-1,  1}  arriba-derecha
+const Position diagonal2 = up   + left;   // {-1, -1}  arriba-izquierda
+const Position diagonal3 = down + right;  // { 1,  1}  abajo-derecha
+const Position diagonal4 = down + left;   // { 1, -1}  abajo-izquierda
 
 
 inline Position directions[4] = {up, down, right, left};
