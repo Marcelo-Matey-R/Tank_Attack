085f12bb5bf7c784214515ae7b4066be8bf7ad2e
Marcelo-Matey-R <queridozapato@gmail.com>
2026-05-19 20:38:36 -0600

    Implementacion de los cambios

--- Logic/src/Map.cpp
+++ Logic/src/Map.cpp
@@ -6,15 +6,17 @@
 
 #pragma region Constructos and Destructor
 
-Map::Map()
-{
+Map::Map(){
+    InitMap();
+}
+
+void Map::InitMap(){
     std::cout << "Creando arrays de tanques..." << std::endl;
     tanksPlayer0 = new Tank *[4];
     tanksPlayer1 = new Tank *[4];
 
     std::cout << "Inicializando arrays a nullptr..." << std::endl;
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++){
         tanksPlayer0[i] = nullptr;
         tanksPlayer1[i] = nullptr;
     }
@@ -33,42 +35,50 @@ Map::Map()
     std::cout << "Constructor completado" << std::endl;
 }
 
-Map::~Map()
-{
-    if (tanksPlayer0 != nullptr)
-    {
-        delete tanksPlayer0[0], tanksPlayer0[1], tanksPlayer0;
+void Map::ResetMap(){
+    if (tanksPlayer0 != nullptr){
+        for(int i = 0; i < 4; i++){
+            if(tanksPlayer0[i] == nullptr) continue;
+            delete tanksPlayer0[i];
+            tanksPlayer0[i] = nullptr;
+        }
+        delete[] tanksPlayer0;  // ← delete[] para arrays
+        tanksPlayer0 = nullptr;
     }
-    if (tanksPlayer1 != nullptr)
-    {
-        delete tanksPlayer1[0], tanksPlayer1[1], tanksPlayer1;
+    if (tanksPlayer1 != nullptr){
+        for(int i = 0; i < 4; i++){
+            if(tanksPlayer0[i] == nullptr) continue;
+            delete tanksPlayer1[i];
+            tanksPlayer1[i] = nullptr;
+        }
+        delete[] tanksPlayer1;
+        tanksPlayer1 = nullptr;
     }
-    for (int i = 0; i < GRID_ROWS; i++)
-    {
-        for (int j = 0; j < GRID_COLS; j++)
-        {
+    for (int i = 0; i < GRID_ROWS; i++){
+        for (int j = 0; j < GRID_COLS; j++){
             if (map[i][j] != nullptr)
                 delete map[i][j];
+                map[i][j] = nullptr;
         }
     }
-    delete[] map;
+}
+
+Map::~Map(){
+    ResetMap();
 }
 
 #pragma endregion
 
 #pragma region Create Map
 
-void Map::FillMap()
-{
+void Map::FillMap(){
     std::cout << "SetTankOfPlayer1..." << std::endl;
     SetTankOfPlayer1();
     std::cout << "SetTankOfPlayer2..." << std::endl;
     SetTankOfPlayer2();
     std::cout << "Llenando mapa con obstaculos..." << std::endl;
-    for (int i = 0; i < GRID_ROWS; i++)
-    {
-        for (int j = 0; j < GRID_COLS; j++)
-        {
+    for (int i = 0; i < GRID_ROWS; i++){
+        for (int j = 0; j < GRID_COLS; j++){
             if (map[i][j] != nullptr)
                 continue;
             int val = binary(gen);
@@ -81,125 +91,49 @@ void Map::FillMap()
     std::cout << "FillMap completado" << std::endl;
 }
 
+void Map::SpawnTank(Tank* tank, int idx, int idPlayer){
+    Position pos;
+    do{
+        pos.i = disRows(gen);
+        pos.j = disCols(gen);
+    }while(!IsNullPlace(pos));
+
+    tank->SetPosition(pos);
+    map[pos.i][pos.j] = tank;
+
+    if(idx == 0){
+        tanksPlayer0[idx] = tank;
+    }
+    else tanksPlayer1[idx] = tank;
+}
+
+
 void Map::SetTankOfPlayer1()
 {
-    // Tanque azul 1
-    Position pos1;
-    do
-    {
-        pos1.i = disRows(gen);
-        pos1.j = disCols(gen);
-    } while (!IsNullPlace(pos1));
-
-    Tank *tankBlue1 = new TankLightBlueAndBlue(ColorTank::BLUE);
-    tanksPlayer0[0] = tankBlue1;
-    tankBlue1->SetPosition(pos1);
-    map[pos1.i][pos1.j] = tankBlue1;
-
-    // Tanque azul 2
-    Position pos2;
-    do
-    {
-        pos2.i = disRows(gen);
-        pos2.j = disCols(gen);
-    } while (!IsNullPlace(pos2));
-
-    Tank *tankBlue2 = new TankLightBlueAndBlue(ColorTank::BLUE);
-    tanksPlayer0[1] = tankBlue2;
-    tankBlue2->SetPosition(pos2);
-    map[pos2.i][pos2.j] = tankBlue2;
-
-    // Tanque rojo 1
-    Position pos3;
-    do
-    {
-        pos3.i = disRows(gen);
-        pos3.j = disCols(gen);
-    } while (!IsNullPlace(pos3));
-
-    Tank *tankRed1 = new TankYellowAndRed(ColorTank::RED);
-    tanksPlayer0[2] = tankRed1;
-    tankRed1->SetPosition(pos3);
-    map[pos3.i][pos3.j] = tankRed1;
-
-    // Tanque rojo 2
-    Position pos4;
-    do
-    {
-        pos4.i = disRows(gen);
-        pos4.j = disCols(gen);
-    } while (!IsNullPlace(pos4));
-
-    Tank *tankRed2 = new TankYellowAndRed(ColorTank::RED);
-    tanksPlayer0[3] = tankRed2;
-    tankRed2->SetPosition(pos4);
-    map[pos4.i][pos4.j] = tankRed2;
+    SpawnTank(new TankLightBlueAndBlue(ColorTank::BLUE), 0, 0);
+    SpawnTank(new TankLightBlueAndBlue(ColorTank::BLUE), 1, 0);
+    SpawnTank(new TankYellowAndRed(ColorTank::RED), 2, 0);
+    SpawnTank(new TankYellowAndRed(ColorTank::RED), 3, 0);
 }
 
-void Map::SetTankOfPlayer2()
-{
-    // Tanque celeste 1
-    Position pos1;
-    do
-    {
-        pos1.i = disRows(gen);
-        pos1.j = disCols(gen);
-    } while (!IsNullPlace(pos1));
-
-    Tank *tankLightBlue1 = new TankLightBlueAndBlue(ColorTank::LIGTHBLUE);
-    tanksPlayer1[0] = tankLightBlue1;
-    tankLightBlue1->SetPosition(pos1);
-    map[pos1.i][pos1.j] = tankLightBlue1;
-
-    // Tanque celeste 2
-    Position pos2;
-    do
-    {
-        pos2.i = disRows(gen);
-        pos2.j = disCols(gen);
-    } while (!IsNullPlace(pos2));
-
-    Tank *tankLightBlue2 = new TankLightBlueAndBlue(ColorTank::LIGTHBLUE);
-    tanksPlayer1[1] = tankLightBlue2;
-    tankLightBlue2->SetPosition(pos2);
-    map[pos2.i][pos2.j] = tankLightBlue2;
-
-    // Tanque amarillo 1
-    Position pos3;
-    do
-    {
-        pos3.i = disRows(gen);
-        pos3.j = disCols(gen);
-    } while (!IsNullPlace(pos3));
-
-    Tank *tankYellow1 = new TankYellowAndRed(ColorTank::YELLOW);
-    tanksPlayer1[2] = tankYellow1;
-    tankYellow1->SetPosition(pos3);
-    map[pos3.i][pos3.j] = tankYellow1;
-
-    // Tanque amarillo 2
-    Position pos4;
-    do
-    {
-        pos4.i = disRows(gen);
-        pos4.j = disCols(gen);
-    } while (!IsNullPlace(pos4));
-
-    Tank *tankYellow2 = new TankYellowAndRed(ColorTank::YELLOW);
-    tanksPlayer1[3] = tankYellow2;
-    tankYellow2->SetPosition(pos4);
-    map[pos4.i][pos4.j] = tankYellow2;
+void Map::SetTankOfPlayer2(){
+    SpawnTank(new TankLightBlueAndBlue(ColorTank::LIGTHBLUE), 0, 1);
+    SpawnTank(new TankLightBlueAndBlue(ColorTank::LIGTHBLUE), 1, 1);
+    SpawnTank(new TankYellowAndRed(ColorTank::YELLOW), 2, 1);
+    SpawnTank(new TankYellowAndRed(ColorTank::YELLOW), 3, 1);
 }
 
-bool Map::VerifyConectivity(const Position destinies[8])
-{
+void Map::RestartGame(){
+    ResetMap();
+    InitMap();
+}
+
+bool Map::VerifyConectivity(const Position destinies[8]){
     return Pathfinding::IsConected(map, destinies);
 }
 
-void Map::EliminateObstacles(const std::vector<Position> &pos)
-{
-    for (Position p : pos)
-    {
+void Map::EliminateObstacles(const std::vector<Position> &pos){
+    for (Position p : pos){
         EliminateObstacle(p);
     }
 }
@@ -230,54 +164,45 @@ void Map::CreateConectivity(){
 
 #pragma region Helpers
 
-bool Map::IsFLoor(const Position &pos)
-{
+bool Map::IsFLoor(const Position &pos){
     if (map[pos.i][pos.j]->GetType() != TypeElement::Floor)
         return false;
     return true;
 }
 
-bool Map::IsObstacle(const Position &pos)
-{
+bool Map::IsObstacle(const Position &pos){
     if (map[pos.i][pos.j]->GetType() != TypeElement::Obstacle)
         return false;
     return true;
 }
 
-bool Map::IsTank(const Position &pos)
-{
+bool Map::IsTank(const Position &pos){
     if (map[pos.i][pos.j]->GetType() != TypeElement::Tank)
         return false;
     return true;
 }
 
-bool Map::IsNullPlace(const Position &pos)
-{
+bool Map::IsNullPlace(const Position &pos){
     if (map[pos.i][pos.j] == nullptr)
         return true;
     return false;
 }
 
-bool Map::IsPositionOcupied(const Position &pos)
-{
+bool Map::IsPositionOcupied(const Position &pos){
     return !IsFLoor(pos) && !IsNullPlace(pos);
 }
 
-bool Map::IsRealPosition(const Position &pos)
-{
+bool Map::IsRealPosition(const Position &pos){
     return (pos.i >= 0 && pos.i < GRID_ROWS && pos.j >= 0 && pos.j < GRID_COLS);
 }
 
 bool Map::IsRealPlayer(int id) { return (id == 0 || id == 1); }
 
-void Map::EliminateObstacle(const Position &pos)
-{
-    if (!IsRealPosition(pos))
-    {
+void Map::EliminateObstacle(const Position &pos){
+    if (!IsRealPosition(pos)){
         throw std::invalid_argument("La posicion no puede ser negativa o sobrepasar los limites del mapa");
     }
-    if (!IsObstacle(pos))
-    {
+    if (!IsObstacle(pos)){
         throw std::invalid_argument("La posicion no corresponde a un obstaculo");
     }
 
@@ -285,40 +210,32 @@ void Map::EliminateObstacle(const Position &pos)
     map[pos.i][pos.j] = new Floor(false);
 }
 
-void Map::EliminateTank(Tank *tank, const Position &pos)
-{
-    if (!IsRealPosition(pos))
-    {
+void Map::EliminateTank(Tank *tank, const Position &pos){
+    if (!IsRealPosition(pos)){
         throw std::invalid_argument("La posicion no puede ser negativa o sobrepasar los limites del mapa");
     }
-    if (!IsTank(pos))
-    {
+    if (!IsTank(pos)){
         throw std::invalid_argument("La posicion no corresponde a un tanque");
     }
-    if (tank->IsAlive())
-    {
+    if (tank->IsAlive()){
         throw std::invalid_argument("No se puede eliminar un tanque vivo");
     }
 
     int player = -1;
     int idx = -1;
 
-    for (int i = 0; i < 4; i++)
-    {
-        if (tanksPlayer0[i] == tank)
-        {
+    for (int i = 0; i < 4; i++){
+        if (tanksPlayer0[i] == tank){
             player = 0;
             idx = i;
         }
-        else if (tanksPlayer1[i] == tank)
-        {
+        else if (tanksPlayer1[i] == tank){
             player = 1;
             idx = i;
         }
     }
 
-    if (player == -1 || idx == -1)
-    {
+    if (player == -1 || idx == -1){
         throw std::invalid_argument("El tanque no existe en el mapa ni en la lista de jugadores");
     }
 
@@ -331,39 +248,32 @@ void Map::EliminateTank(Tank *tank, const Position &pos)
     map[pos.i][pos.j] = new Floor(true);
 }
 
-void Map::MoveTank(Tank *tank, Position &destination)
-{
-    if (!IsRealPosition(destination))
-    {
+void Map::MoveTank(Tank *tank, Position &destination){
+    if (!IsRealPosition(destination)){
         throw std::invalid_argument("La posicion no puede ser negativa o sobrepasar los limites del mapa");
     }
-    if (!tank->IsAlive())
-    {
+    if (!tank->IsAlive()){
         throw std::invalid_argument("No se puede eliminar un tanque vivo");
     }
 
-    if (IsPositionOcupied(destination))
-    {
+    if (IsPositionOcupied(destination)){
         Position t = tank->GetPosition();
         Position newDestiny;
         int cost = INF;
-        for (auto direction : allDirections)
-        {
+        for (auto direction : allDirections){
             Position tmp = destination + direction;
             if (!IsRealPosition(tmp) || IsPositionOcupied(tmp))
                 continue;
 
             int dx = abs(t.i - tmp.i);
             int dy = abs(t.j - tmp.j);
-            if ((dx + dy) < cost)
-            {
+            if ((dx + dy) < cost){
                 cost = dx + dy;
                 newDestiny = tmp;
             }
         }
 
-        if (cost == INF)
-        {
+        if (cost == INF){
             throw std::invalid_argument("No se permiten destinos previamente ocupados");
         }
 
@@ -379,55 +289,45 @@ void Map::MoveTank(Tank *tank, Position &destination)
 
 #pragma region Gets
 
-Element *Map::GetElementAt(const Position &pos)
-{
-    if (!IsRealPosition(pos))
-    {
+Element *Map::GetElementAt(const Position &pos){
+    if (!IsRealPosition(pos)){
         throw std::invalid_argument("La posicion no puede ser negativa o sobrepasar los limites del mapa");
     }
     return map[pos.i][pos.j];
 }
 
-Tank *Map::GetTankIn(const Position &pos)
-{
-    if (!IsRealPosition(pos))
-    {
+Tank *Map::GetTankIn(const Position &pos){
+    if (!IsRealPosition(pos)){
         throw std::invalid_argument("La posicion no puede ser negativa o sobrepasar los limites del mapa");
     }
-    if (!IsTank(pos))
-    {
+    if (!IsTank(pos)){
         return nullptr;
     }
     return static_cast<Tank *>(map[pos.i][pos.j]);
 }
 
-Tank *Map::GetTankOfPlayer(int idPlayer, ColorTank color)
-{
-    if (!IsRealPlayer(idPlayer))
-    {
+Tank *Map::GetTankOfPlayer(int idPlayer, ColorTank color){
+    if (!IsRealPlayer(idPlayer)){
         throw std::invalid_argument("El id del jugador no existe");
     }
-    switch (idPlayer)
-    {
-    case 0:
-        if (color == ColorTank::BLUE)
-            return tanksPlayer0[1];
-        return tanksPlayer0[0];
+    switch (idPlayer){
+        case 0:
+            if (color == ColorTank::BLUE)
+                return tanksPlayer0[1];
+            return tanksPlayer0[0];
 
-    case 1:
-        if (color == ColorTank::LIGTHBLUE)
-            return tanksPlayer1[1];
-        return tanksPlayer1[0];
+        case 1:
+            if (color == ColorTank::LIGTHBLUE)
+                return tanksPlayer1[1];
+            return tanksPlayer1[0];
 
-    default:
-        return nullptr;
+        default:
+            return nullptr;
     }
 }
 
-Tank **Map::GetTanksOfPlayer(int idPlayer)
-{
-    if (!IsRealPlayer(idPlayer))
-    {
+Tank **Map::GetTanksOfPlayer(int idPlayer){
+    if (!IsRealPlayer(idPlayer)){
         throw std::invalid_argument("El id del jugador no existe");
     }
 
@@ -437,8 +337,7 @@ Tank **Map::GetTanksOfPlayer(int idPlayer)
     return tanksPlayer1;
 }
 
-void Map::GetMap(Element *(&outMap)[GRID_ROWS][GRID_COLS])
-{
+void Map::GetMap(Element *(&outMap)[GRID_ROWS][GRID_COLS]){
     for (int i = 0; i < GRID_ROWS; i++)
         for (int j = 0; j < GRID_COLS; j++)
             outMap[i][j] = map[i][j];
